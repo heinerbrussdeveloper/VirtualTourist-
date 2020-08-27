446f7000fafa878c3a4f0207182ba7f165cdc016
Heiner Bruss <heiner.bruss@web.de>
2020-08-27 16:04:26 +0200

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Heiner Bruß on 27.08.20.
+//  Copyright © 2020 Heiner Bruß. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
